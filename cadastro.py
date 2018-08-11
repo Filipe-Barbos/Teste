4e19267a3ab01b3f53e5ea75eb7b55439502ab92
Filipe-Barbos <filip.barbos@gmail.com>
2018-08-10 21:52:21 -0300

    Primeiro commit deste arquivo

---
+++ cadastro.py
@@ -0,0 +1,18 @@
+dados = list()
+pessoas = list()
+
+while True:
+    dados.append(str(input('Nome:')).strip())
+    dados.append(float(input('Peso: ')))
+    pessoas.append(dados[:])
+    dados.clear()
+    resp = str(input('Deseja continuar? (S/N): ')).strip().upper()
+    while resp not in ('SN'):
+        print('Resposta Inválida. Tente Novamente.')
+        resp = str(input('Deseja continuar? (S/N): ')).strip().upper()
+    
+    if resp == 'N':
+        break
+
+print('=-' * 30)
+print(f'Foram cadastradas {len(pessoas)} pessoa(s).')
